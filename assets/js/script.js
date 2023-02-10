414b0ad7f0ece7a235e3b60a075c2b3794ccd4ad
junnel308 <junnelpadilla08@gmail.com>
2023-02-09 18:10:18 -0800

    added first second prompt for adding lowercase

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,9 +21,31 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword () {
     passwordLength = prompt ("How many characters do you want to add? (please choose between 8-12 charaacters)");
-    if(passwordLength >= 8 || passwordLength <=12) {
-        if(window.confirm("Please confirm the amount inputed:" + " " + passwordLength + " " + "characters")){
+    if(passwordLength >= 8 && passwordLength <=12) {
+        if(window.confirm("Please confirm the amount inputted:" + " " + passwordLength + " " + "characters")){
             alert(passwordLength + " " +"characters added!");
+        } else {
+            alert("Please choose a number between 8-12");
         }
+    } else {
+        alert("Characters must be a minimum of 8 and no more than 12");
+    }
+
+    /* Prompts user to add or dont add LOWERCASE to password */
+    firstEntry = prompt ("Would you like to include lowercase characters? ('Y' or 'N')");
+    if(firstEntry == "y" || firstEntry == "Y" || firstEntry == "yes" || firstEntry == "YES") {
+        if (window.confirm("Please press 'OK' to confirm, otherwise hit 'CANCEL'")) {
+            alert ("Lower Case will be Added!");
+        } else {
+            alert("Lower Case will NOT be added!");
+        } 
+        } else if (firstEntry == "n" || firstEntry == "N" || firstEntry == "no" || firstEntry == "NO") {
+            if (window.confirm("Please press 'OK' to confirm, otherwise hit 'CANCEL'")) {
+                alert ("Lower Case will NOT be Added!");
+            } else {
+                alert ("Lower Case will be Added!");
+            }
+    } else {
+        alert("Invalid input. Please Try Again. ");
     }
 }
