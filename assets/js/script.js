0708808defbb11410950ae601f17a69bf568c4fd
junnel308 <junnelpadilla08@gmail.com>
2023-02-11 12:19:37 -0800

    added a function if user did not choose one criteria, password wont generate

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,10 @@ var numberChar = "1234567890";
 var specialChar = ",./;'[]`!@#$%^&*()_+=-|}:?><";
 
 function writePassword () {
-    var passwordLength;
-    var firstEntry;
-    var secondEntry;
-    var thirdEntry;
-    var fourthEntry;
     var passwordGen = generatePassword();
     var passwordText = document.querySelector("#password");
 
+    
     passwordText.value = passwordGen;
 }
 
@@ -23,7 +19,7 @@ function generatePassword () {
     /* While loop to re-prompt user is input is invalid or the number chosen is not between 8-12 */
     while (true) {
         /* Prompts user to choose number between 8-12 to include in password */
-        passwordLength = prompt ("How many characters do you want to add? (please choose between 8-12 characters)");
+     var passwordLength = prompt ("How many characters do you want to add? (please choose between 8-12 characters)");
         if(passwordLength >= 8 && passwordLength <=12) {
             if(window.confirm("Please confirm the amount inputed:" + " " + passwordLength + " " + "characters")){
                 alert(passwordLength + " " +"characters added!");
@@ -40,8 +36,8 @@ function generatePassword () {
     }
 
     /* Prompts user to add or dont add LOWERCASE to password */
-    firstEntry = confirm ("Would you like to include LOWERCASE letters to your password? (press 'OK' to add or press 'Cancel' to not add)");
-        if(firstEntry === true ) {
+    var firstEntry = confirm ("Would you like to include LOWERCASE letters to your password? (press 'OK' to add or press 'Cancel' to not add)");
+        if(firstEntry == true ) {
                 alert ("LOWERCASE will be Added!");
                 lowerChar;
             } else {
@@ -50,8 +46,8 @@ function generatePassword () {
             } 
 
     /* Prompts user to add or dont add UPPERCASE to password */
-    secondEntry = confirm ("Would you like to include UPPERCASE letters to your password? (press 'OK' to add or press 'Cancel' to not add)");
-        if(secondEntry === true ) {
+   var secondEntry = confirm ("Would you like to include UPPERCASE letters to your password? (press 'OK' to add or press 'Cancel' to not add)");
+        if(secondEntry == true ) {
                 alert ("UPPERCASE will be Added!");
                 upperChar;
             } else {
@@ -60,8 +56,8 @@ function generatePassword () {
             } 
 
     /* Prompts user to add or dont add NUMBERS to password */
-    thirdEntry = confirm ("Would you like to include NUMBERS to your password? (press 'OK' to add or press 'Cancel' to not add)");
-        if(thirdEntry === true ) {
+    var thirdEntry = confirm ("Would you like to include NUMBERS to your password? (press 'OK' to add or press 'Cancel' to not add)");
+        if(thirdEntry == true ) {
                 alert ("NUMBERS will be Added!");
                 numberChar;
             } else {
@@ -69,40 +65,22 @@ function generatePassword () {
                 numberChar = " ";
             } 
 
-    /* While loop to re-prompt if input is invalid */
-    while (true) {
-        /* Prompts user to add or dont add SPECIAL CHARACTERS to password */
-        fourthEntry = prompt ("Would you like to include SPECIAL CHARACTERS? ('Y' or 'N')");
-        if(fourthEntry == "y" || fourthEntry  == "Y" || fourthEntry == "yes" || fourthEntry  == "YES"){
-            if (window.confirm("Please press 'OK' to confirm, otherwise hit 'CANCEL'")) {
+    /* Prompts user to add or dont add SPECIAL CHARACTERS to password */
+    var fourthEntry = confirm ("Would you like to include SPECIAL CHARACTERS to your password? (press 'OK' to add or press 'Cancel' to not add)");
+        if(fourthEntry == true ) {
                 alert ("SPECIAL CHARACTERS will be Added!");
                 specialChar;
-                break;
             } else {
                 alert("SPECIAL CHARACTERS will NOT be added!");
                 specialChar = " ";
-                break;
             } 
-            } else if (fourthEntry  == "n" || fourthEntry  == "N" || fourthEntry  == "no" || fourthEntry  == "NO") {
-                if (window.confirm("Please press 'OK' to confirm, otherwise hit 'CANCEL'")) {
-                    alert ("SPECIAL CHARACTERS will NOT be Added!");
-                    specialChar = " ";
-                    break;
-                } else {
-                    alert ("SPECIAL CHARACTERS will be Added!");
-                    specialChar;
-                    break;
-                }
-        } else {
-                alert("Invalid Input. Please Try Again. ");
-        }
-    }
 
-        /* re-prompts user to the first question if no criteria is chosen by the user */
-        if (firstEntry + secondEntry + thirdEntry + fourthEntry == " ") {
-            alert (" Must include at least one criteria. Please try again.")
-            generatePassword();
-        } 
+    /* re-prompts user to the first question if no criteria is chosen by the user */
+    if (firstEntry == false && secondEntry == false && thirdEntry == false && fourthEntry == false) {
+        alert ("Must include at least one criteria. Click 'Generate Password' button to try again.");
+        window.location.reload();
+    } 
+
      
 
     /* variable that includes all the lowercase, uppercase, numbers and special characters */
